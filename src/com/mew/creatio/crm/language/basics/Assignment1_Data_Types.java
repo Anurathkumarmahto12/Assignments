62b0c43ce3399ad708a97ed481ad6fe28cc682f0
Anurath Kumar Mahto <anurathkumar14@gmail.com>
2025-04-09 11:48:11 +0530

    Assignment1_Datatypes

---
+++ src/com/mew/creatio/crm/language/basics/Assignment1_Data_Types.java
@@ -0,0 +1,99 @@
+package com.mew.creatio.crm.language.basics;
+
+public class Assignment1_Data_Types {
+
+	public static void main(String[] args) 
+	{
+		//1. Temperature of a city in degrees Celsius: 25.5
+	   float Temperature= 25.5f;
+	   System.out.println("1. Temperature of a city in degrees Celsius : " + Temperature);
+	   
+	   //2. Whether a customer has placed an order: true or false 
+	   boolean order= true;
+	   System.out.println("2. Whether a customer has placed an order : "  + order);
+	   
+	   //3. Person's phone number: "123-456-7890"
+	   String phoneNumber= "123-456-7890";
+	   System.out.println("3. Person's phone number :" + phoneNumber);
+	   
+	   //4. Amount of money in a customer's bank account: 1000.50
+	   float money= 1000.50f;
+	   System.out.println("4. Amount of money in a customer's bank account : " + money);
+	   
+	   //5. Person's email address: "john.doe@example.com"
+	   String email="john.doe@example.com";
+	   System.out.println("5. Person's email address : " + email);
+	   
+	   //6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+	   double a = 37.7749;
+	   double b = -122.4194;
+	   System.out.println("6. Coordinates of a location (latitude, longitude):" + a +"," + b);
+	   
+	   //7. Person's marital status: true or false
+	   boolean status=false;
+	   System.out.println("7. Person's marital status: true or false :" + status);
+	   
+	   //8. Person's occupation: "Software Engineer"
+	   String occupation="Software Engineer";
+	   System.out.println("8. Person's occupation :" + occupation);
+	   
+	   //9. Person's favourite colour: "Blue"
+	   String colour="Blue";
+	   System.out.println("9. Person's favourite colour: " + colour);
+	   
+	   //10.Current year: 2023
+	   int year=2023;
+	   System.out.println("10. Current Year:" + year);
+	   
+	   //11. Number of followers on a social media platform: 1,000,000
+	   int follow = 100000;
+	   System.out.println("11. Number of followers on a social media platform:" + follow);  
+	   
+	   //12. Rating of a movie: 7.5
+	   float movie=7.5f;
+	   System.out.println("12. Rating of a movie:" + movie);
+	   
+	   //13. Person's blood type: 'A'
+	   char blood='A';
+	   System.out.println("13. Person's blood type:" + blood);
+	   
+	   //14.Title of a book: "To Kill a Mockingbird"
+	   String title="To Kill a Mockingbird";
+	   System.out.println("14. Title of a book:" + title);
+	   
+	   //15.Number of employees in a company: 500
+	   int no_of_employee=500;
+	   System.out.println("15. Number of employees in a company:" + no_of_employee);
+	   
+	   //16.Time of an event: 2:30 PM
+	   String eventTime = "2:30 PM";
+	   System.out.println("16. Time of the event: " + eventTime);
+	   
+	   //17.Name of a country: "United States"
+	   String country = "United States";
+	   System.out.println("17. Name of a country:" + country);
+       
+       //18.Person's eye color: "Brown"
+       String eyeColor = "Brown";
+       System.out.println("18. Person's eye color:" + eyeColor);
+       
+       // 19.Person's birthplace: "New York City"
+       String birthplace = "New York City";
+       System.out.println("19. Person's birthplace:" + birthplace);
+       
+       // 20.Distance between two cities: 200.5 
+       double distance = 200.5;
+       System.out.println("20. Distance between two cities:" + distance);
+	   
+	   
+	   
+	   
+	   /*
+		3. Person's phone number: "123-456-7890"
+		4. Amount of money in a customer's bank account: 1000.50
+		5. Person's email address: "john.doe@example.com"
+		6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+*/
+	}
+
+}
